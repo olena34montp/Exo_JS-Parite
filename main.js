9c9587feb04c0c8b13ccdb88e5c1a70fb898bab7
Olena ILCHENKO <olena.ilchenko@etud.gretacfa-montpellier.fr>
2023-01-14 10:38:25 +0100

    ajout Exo_JS-Parite

---
+++ main.js
@@ -0,0 +1,9 @@
+function isEven (num) {
+    return num % 2 == 0; 
+};
+
+console.log(isEven(1)); //false
+console.log(isEven(2)); //true
+console.log(isEven(10)); //true
+console.log(isEven(34)); //true
+console.log(isEven(99)); //false
